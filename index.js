e4ff4645c1d6ac9f506534aea7db354d3cfaa670
José rennyer <joserennyer1000@gmail.com>
2024-12-04 11:13:52 -0300

    versão dois do desafio

--- index.js
+++ index.js
@@ -1,27 +1,36 @@
-const heroName = "Gen"
-let heroExp = 9000
-let heroLevel
+const heroes = [
+["Gon", 5000], 
+["Jun", 2500], 
+["Mary", 7600], 
+["Tyler", 10002], 
+["July", 400]
+]
 
 
-if(heroExp <= 1000) {
-    heroLevel = "Ferro"
-} else if(heroExp > 1000 && heroExp <= 2000) {
-    heroLevel = "Bronze"
-} else if(heroExp > 2000 && heroExp <= 5000) {
-    heroLevel = "Prata"
-} else if(heroExp > 5000 && heroExp <= 6000) {
-    heroLevel = "Ouro"
-} else if(heroExp > 6000 && heroExp <= 7000) {
-    heroLevel = "Platina"
-} else if(heroExp > 7000 && heroExp <= 8000) {
-    heroLevel = "Diamante"
-} else if(heroExp > 8000 && heroExp <= 9000) {
-    heroLevel = "Ascendente"
-} else if(heroExp > 9000 && heroExp <= 10000) {
-    heroLevel = "Imortal"
-} else {
-    heroLevel = "Radiante"
+for(let i = 0; i < heroes.length; i++) {
+    let heroExp = heroes[i][1]
+    if(heroExp <= 1000) {
+        heroes[i].push("Ferro")
+    } else if(heroExp > 1000 && heroExp <= 2000) {
+        heroes[i].push("Bronze")
+    } else if(heroExp > 2000 && heroExp <= 5000) {
+        heroes[i].push("Prata")
+    } else if(heroExp > 5000 && heroExp <= 6000) {
+        heroes[i].push("Ouro")
+    } else if(heroExp > 6000 && heroExp <= 7000) {
+        heroes[i].push("Platina")
+    } else if(heroExp > 7000 && heroExp <= 8000) {
+        heroes[i].push("Diamante")
+    } else if(heroExp > 8000 && heroExp <= 9000) {
+        heroes[i].push("Ascendente")
+    } else if(heroExp > 9000 && heroExp <= 10000) {
+        heroes[i].push("Imortal")
+    } else {
+        heroes[i].push("Radiante")
+    }
 }
 
 
-console.log("O héroi de nome " + heroName + " está no nível de " + heroLevel)
+for(let i = 0; i < heroes.length; i++) {
+    console.log("O héroi de nome " + heroes[i][0] + " está no nível de " + heroes[i][2])
+}
